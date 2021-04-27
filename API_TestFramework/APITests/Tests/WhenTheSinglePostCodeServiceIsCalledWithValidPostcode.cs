a5b4d02eee65a1a5a6eb7c5a2748ca6797221dcf
BarathNallusamy <nallu.baray@gmail.com>
2021-04-27 15:33:45 +0100

    initial tests passed with singlepostcodeservice

---
+++ API_TestFramework/APITests/Tests/WhenTheSinglePostCodeServiceIsCalledWithValidPostcode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using API_App;
+using NUnit.Framework;
+
+namespace APITests.Tests
+{
+    public class WhenTheSinglePostCodeServiceIsCalledWithValidPostcode
+    {
+        SinglePostCodeService _singlePostCodeService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _singlePostCodeService = new SinglePostCodeService();
+            _singlePostCodeService.MakeRequest("EC2Y 5AS");
+        }
+
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_singlePostCodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+        }
+
+        [Test]
+        public void CorrectPostcodeIsReturned()
+        {
+            var result = _singlePostCodeService.ResponseContent["result"]["postcode"].ToString();
+            Assert.That(result, Is.EqualTo("EC2Y 5AS"));
+        }
+    }
+}
---
+++ API_TestFramework/API_App/SinglePostCodeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace API_App
+{
+    public class SinglePostCodeService
+    {
+        #region Properties
+        //REstsharp object which handles communication with API
+        public RestClient Client;
+        // A Newtonsoft object representing the JSON response
+        public JObject ResponseContent{ get; set; }
+        //Restsharp response Object
+        public IRestResponse RestResponse { get; set; }
+        //The postcode used in the API request
+        public string PostcodeSelected { get; set; }
+        #endregion
+
+        //constructor - creates the rest client object
+        public SinglePostCodeService()
+        {
+            Client = new RestClient { BaseUrl = new Uri(AppConfigReader.BaseUrl) };
+        }
+
+        public void MakeRequest(string postcode)
+        {
+            //setup the request
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            PostcodeSelected = postcode;
+
+            //define request resource path,(changing to lowercase and removing the spaces)
+            request.Resource = $"postcodes/{postcode.ToLower().Replace(" ", "")}";
+
+            //make the request
+            RestResponse = Client.Execute(request);
+
+            //parse json into a JObject
+            ResponseContent = JObject.Parse(RestResponse.Content);
+        }
+    }
+}
